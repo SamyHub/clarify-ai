0b8a6667f772a24040d0867f808e3fc4e4fcb6ad
SamyHub <119066825+SamyHub@users.noreply.github.com>
2023-10-22 14:46:49 +0100

    Update SCUI_Variables.py

--- scripts/SCUI_Variables.py
+++ scripts/SCUI_Variables.py
@@ -16,7 +16,7 @@ __location__ = os.path.dirname(os.path.realpath(__file__))
 #############################################################################
 # Different variables classes
 class OpenAIkey:
-    OPENAI_API_KEY = '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'
+    OPENAI_API_KEY = ''
 
 class Icons:
     iconsPath = os.path.join(__location__,'icons')
